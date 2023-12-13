0fd263c89d79ac558d434bed5b11f585870743b7
Sanjay Khanna S <107598515+Sanjaykhanna17@users.noreply.github.com>
2023-12-13 12:14:50 +0530

    Update Hello.java
    Program was Updated

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello{
-public static void main(String[] arg){
-System.out.println("Hello World");
-}
+  public static void main(String[] arg){
+    System.out.println("Hello World");
+    System.out.Println("This is Sanjay Khanna S ");
+  }
 }
